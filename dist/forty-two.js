f8162e4b41cdd28fee29f7835b30e4e2d392b290
veob <veob.dev@gmail.com>
2015-09-02 18:01:47 +0300

    start refactor

--- dist/forty-two.js
+++ dist/forty-two.js
@@ -27,6 +27,10 @@
 			'17': 'семнадцать',
 			'18': 'восемнадцать',
 			'19': 'девятнадцать',
+			'tens': [
+				'', '', 'двадцать', 'тридцать', 'сорок', 'пятьдесят',
+				'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'
+			],
 			'20': 'двадцать',
 			'30': 'тридцать',
 			'40': 'сорок',
@@ -35,32 +39,30 @@
 			'70': 'семьдесят',
 			'80': 'восемьдесят',
 			'90': 'девяносто',
-			'100': 'сто',
-			'200': 'двести',
-			'300': 'триста',
-			'400': 'четыреста',
-			'500': 'пятьсот',
-			'600': 'шестьсот',
-			'700': 'семьсот',
-			'800': 'восемьсот',
-			'900': 'девятьсот',
-			'1000': {pluralForms: ['тысяча', 'тысячи', 'тысяч']},
-			'1000000': {pluralForms: ['миллион', 'миллиона', 'миллионов']},
-			'1000000000': {pluralForms: ['миллиард', 'миллиарда', 'миллиардов']}
+			'hundreds': [
+				'', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот',
+				'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'
+			],
+			'tenInPower': {
+				'3': {pluralForms: ['тысяча', 'тысячи', 'тысяч']},
+				'6': {pluralForms: ['миллион', 'миллиона', 'миллионов']},
+				'9': {pluralForms: ['миллиард', 'миллиарда', 'миллиардов']}
+			}
 		};
 
-		this.parse = function(number, rank) {
+		this._parse = function(memo, number, rank) {
 			var result = [];
 			if (number.toString().length === 3) {
 				var hundred = (~~(number / 100) * 100);
+
 				//parse hundreds
 				result.push(this.locales.ru[hundred]);
 				number = number % 100;
 			}
 
 			if (number >= 20) {
-				var dozen = (~~(number / 10) * 10);
-				result.push(this.locales.ru[dozen]);
+				var ten = (~~(number / 10) * 10);
+				result.push(this.locales.ru[ten]);
 				number = number.toString()[1];
 			}
 
@@ -72,17 +74,47 @@
 			result.push(this.locales.ru[number][gender] || this.locales.ru[number]);
 
 			if (rank === 2) {
-				result.push(this.plural(number, this.locales.ru[1000].pluralForms));
+				result.push(this.plural(number, this.locales.ru[1000].pluralForms || this.locales.ru[1000]));
 			}
 
 			if (rank === 3) {
-				result.push(this.plural(number, this.locales.ru[1000000].pluralForms));
+				result.push(this.plural(number, this.locales.ru[1000000].pluralForms || this.locales.ru[1000]));
 			}
 
 			return result;
 		};
-	}
 
+		this.parse = function(memo, currentNumber, currentRank, locale) {
+			var numberString = currentNumber.toString();
+			if (numberString.length === 3) {
+				//parse hundreds
+				memo.push(locale.hundreds[numberString[0]]);
+
+				numberString = numberString.slice(1, 3);
+			}
+
+			if (currentNumber >= 20) {
+				memo.push(this.locales.ru[ten]);
+				currentNumber = currentNumber.toString()[1];
+			}
+
+			var gender = 'masculine';
+			if (currentRank === 2) {
+				gender = 'feminine';
+			}
+
+			memo.push(locale[currentNumber][gender] || locale[currentNumber]);
+
+			if (currentRank === 2) {
+				memo.push(this.plural(currentNumber, locale[1000].pluralForms || locale[1000]));
+			}
+
+			if (currentRank === 3) {
+				memo.push(this.plural(currentNumber, locale[1000000].pluralForms || locale[1000]));
+			}
+
+		};
+	}
 	FortyTwo.prototype.wordify = function(number, locale) {
 		number = number.toString();
 
@@ -99,20 +131,18 @@
 			parts.push(number.slice(start, end));
 		}
 
-		console.log('parts', parts);
-
 		var rank = parts.length;
 
 		for (i = partsLength - 1; i >= 0; i--) {
 			var temp = parts[i];
 
 			if (parseInt(temp, 10)) {
-				resultParts = resultParts.concat(this.parse(temp, rank));
+				resultParts = resultParts.concat(this._parse(temp, rank));
 			}
 			rank--;
 		}
 
-		resultParts = resultParts.filter(Boolean);
+		resultParts = filter(resultParts);
 		var result = resultParts.join(' ');
 
 		return result;
@@ -131,6 +161,16 @@
 
 		return wordForms[2];
 	};
+
+	function filter(array) {
+		var result = [];
+		for (var i = 0; i < array.length; i++) {
+			if (array[i]) {
+				result.push(array[i]);
+			}
+		}
+		return result;
+	}
 
 	if (typeof exports !== 'undefined' ) {
 		if (typeof module !== 'undefined' && module.exports ) {
